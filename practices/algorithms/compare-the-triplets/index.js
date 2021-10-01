0bb1b576a565b2a77f49565a58d72f2d13039cc7
‘peter-robert-cole’ <petercole3@gmail.com>
2021-10-01 00:12:18 -0700

    refactor compare-the-triplets

--- practices/algorithms/compare-the-triplets/index.js
+++ practices/algorithms/compare-the-triplets/index.js
@@ -1,17 +1,18 @@
 function compareTriplets(a, b) {
   let tally = [0, 0];
+
   function weigh(count) {
     if (count > 2) return tally;
+
     let dif = a[count] - b[count];
-    if (dif > 0) {
-      tally[0]++;
-    } else if (dif < 0) {
-      tally[1]++;
-    }
-    const newCount = count + 1;
-    weigh(newCount);
+
+    dif > 0 ? tally[0]++ : tally[1]++;
+
+    weigh(count + 1);
   }
+
   weigh(0);
+
   return tally;
 }
 
