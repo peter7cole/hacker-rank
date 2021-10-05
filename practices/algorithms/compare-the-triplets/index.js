1ebb44fe60b2b7d20be7e19c0019c7a9ffb58c73
‘peter-robert-cole’ <petercole3@gmail.com>
2021-10-05 00:47:02 -0700

    refactor helper in compare-the-triplets

--- practices/algorithms/compare-the-triplets/index.js
+++ practices/algorithms/compare-the-triplets/index.js
@@ -1,22 +1,29 @@
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTwo(index, a, b, arr) {
+  if (index > 2) return arr;
+
+  let dif = a[index] - b[index];
+
+  if (dif != 0) dif > 0 ? (arr[0] += 1) : (arr[1] += 1);
+
+  compareTwo(index + 1, a, b, arr);
+}
+
 function compareTriplets(a, b) {
   let tally = [0, 0];
 
-  function weigh(count) {
-    if (count > 2) return tally;
-
-    let dif = a[count] - b[count];
-
-    dif > 0 ? tally[0]++ : tally[1]++;
-
-    weigh(count + 1);
-  }
-
-  weigh(0);
+  compareTwo(0, a, b, tally);
 
   return tally;
 }
 
-console.log(
-  JSON.stringify(compareTriplets([17, 28, 30], [99, 16, 8])) ===
-    JSON.stringify([2, 1])
-);
+// Expect: [1, 1]
+console.log(compareTriplets([2, 1, 3], [2, 2, 2]));
